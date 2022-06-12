81733d41756dc252c0c9975d35119d2f2b8ce4d2
Locomody <67393547+Locomody@users.noreply.github.com>
2022-06-12 16:30:01 +0300

    Week-3 - Fix compilation errors, write tests

--- src/test/scala/karazin/scala/users/group/week3/homework/ProgramSuite.scala
+++ src/test/scala/karazin/scala/users/group/week3/homework/ProgramSuite.scala
@@ -3,6 +3,11 @@ package karazin.scala.users.group.week3.homework
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+import karazin.scala.users.group.week3.homework.model.*
+import karazin.scala.users.group.week3.homework.program.*
+import java.util.UUID
+import scala.util.{Success, Failure}
+
 /*
   Write test for all programs in karazin.scala.users.group.week3.homework.program
 
@@ -12,9 +17,11 @@ import scala.concurrent.Future
  */
 
 class ProgramSuite extends munit.FunSuite:
-  
-  test("successful async test example") {
-    Future {
-      assertEquals(42, 42)
+
+  test("getPostView test") {
+    val postView = getPostView(Post(UUID.randomUUID(),UUID.randomUUID()))
+    postView onComplete {
+      case Success(result) => assertEquals(42, 42)
+      case Failure(error)  => fail("getPostView failed")
     }
   }
--- src/test/scala/karazin/scala/users/group/week3/homework/ServicesSuite.scala
+++ src/test/scala/karazin/scala/users/group/week3/homework/ServicesSuite.scala
@@ -2,6 +2,11 @@ package karazin.scala.users.group.week3.homework
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import karazin.scala.users.group.week3.homework.model.*
+import karazin.scala.users.group.week3.homework.services.*
+import java.util.UUID
+
+import scala.util.{Success, Failure}
 
 /*
   Write test for all service in karazin.scala.users.group.week3.homework.services
@@ -12,11 +17,43 @@ import scala.concurrent.Future
  */
 
 class ServicesSuite extends munit.FunSuite:
-  
-  test("failed async test example") {
-    Future {
-      assertEquals(42, 43)
+
+  test("getUserProfile test") {
+    val userProfile = getUserProfile()
+    userProfile onComplete {
+      case Success(result) => assertEquals(42, 42)
+      case Failure(error)  => fail("getUserProfile failed")
     }
   }
 
-  
+  test("getPosts test") {
+    val posts = getPosts(UUID.randomUUID())
+    posts onComplete {
+      case Success(result) => assertEquals(42, 42)
+      case Failure(error)  => fail("getPosts failed")
+    }
+  }
+
+  test("getComments test") {
+    val comments = getComments(UUID.randomUUID())
+    comments onComplete {
+      case Success(result) => assertEquals(42, 42)
+      case Failure(error)  => fail("getComments failed")
+    }
+  }
+
+  test("getLikes test") {
+    val likes = getLikes(UUID.randomUUID())
+    likes onComplete {
+      case Success(result) => assertEquals(42, 42)
+      case Failure(error)  => fail("getLikes failed")
+    }
+  }
+
+  test("getShares test") {
+    val shares = getShares(UUID.randomUUID())
+    shares onComplete {
+      case Success(result) => assertEquals(42, 42)
+      case Failure(error)  => fail("getShares failed")
+    }
+  }
