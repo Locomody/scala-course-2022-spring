ba5c6f8dd16ab9f392340d9d3b1178711cfc4989
Locomody <67393547+Locomody@users.noreply.github.com>
2022-06-13 17:03:03 +0300

    Week-4 - Fix Compilation and write tests

--- src/test/scala/karazin/scala/users/group/week4/homework/ProgramSuite.scala
+++ src/test/scala/karazin/scala/users/group/week4/homework/ProgramSuite.scala
@@ -3,6 +3,16 @@ package karazin.scala.users.group.week4.homework
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+import karazin.scala.users.group.week4.homework.model._
+import karazin.scala.users.group.week4.homework.program._
+import karazin.scala.users.group.week4.homework.services._
+
+import java.util.UUID
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
+import scala.util.{Try,Success,Failure}
+
 /*
   Write test for all programs in karazin.scala.users.group.week4.homework.program
   Make sure you control custom execution contexts in tests using `before` and `after` logic
@@ -15,8 +25,39 @@ import scala.concurrent.Future
 
 class ProgramSuite extends munit.FunSuite:
 
-  test("successful async test example") {
-    Future {
-      assertEquals(42, 42)
+  def assertTestList[V](list: List[V])(f: V => Unit) =
+    list foreach f
+
+  val sigleThreadPoolContext =
+    new Fixture[ExecutionContext]("files") {
+      var threadpool: ExecutionContextExecutorService = null
+      def apply() = threadpool
+      override def beforeEach(context: BeforeEach): Unit = {
+        threadpool = ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor)
+      }
+      override def afterEach(context: AfterEach): Unit = {
+        // Always gets called, even if test failed.
+        threadpool.shutdown()
+      }
+    }
+  override def munitFixtures = List(sigleThreadPoolContext)
+
+  test("getPostView test") {
+    val post = Post(UUID.randomUUID(), UUID.randomUUID())
+    val getPostViewService  = getPostView(post)(using sigleThreadPoolContext())
+    getPostViewService map { res => res match
+      case PostView(`post`, _, _, _) => assert(true)
+      case _ => fail("getPostView fail")
+    } recover {
+      case error => fail("Exception")
+    }
+  }
+
+  test("getPostViews test") {
+    val getPostsViewService  = getPostsViews()(using sigleThreadPoolContext())
+    getPostsViews() map { list =>
+      assertTestList[PostView](list) { p => assert(true)}
+    } recover {
+      case error => fail("Exception")
     }
   }
